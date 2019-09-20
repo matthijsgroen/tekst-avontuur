81f72c384f6a42eb45b2d0913f78e26cda4856da
Matthijs Groen <matthijs@kabisa.nl>
2019-09-20 09:10:31 +0200

    Refactor use of setColor

--- avontuur-hs/src/Display.hs
+++ avontuur-hs/src/Display.hs
@@ -15,33 +15,28 @@ showText (x:xs) = do
 showText [] = putStrLn ""
 
 printDisplayData :: DisplayData -> IO ()
-printDisplayData (Text text) =
-  case text of
-    "" -> putStrLn ""
-    _ -> showText text
+printDisplayData (Text text) | text == "" = putStrLn ""
+printDisplayData (Text text) = showText text
 
 printDisplayData (Color x) = setColor x
 printDisplayData (Delay amount) = do
   threadDelay $ amount * seconds
 
-setColor :: Int -> IO ()
-setColor 0 = setSGR [SetColor Foreground Dull Black]
-setColor 1 = setSGR [SetColor Foreground Dull Blue]
-setColor 2 = setSGR [SetColor Foreground Dull Green]
-setColor 3 = setSGR [SetColor Foreground Dull Cyan]
-setColor 4 = setSGR [SetColor Foreground Dull Red]
-setColor 5 = setSGR [SetColor Foreground Dull Magenta]
-setColor 6 = setSGR [SetColor Foreground Dull Yellow]
-setColor 7 = setSGR [SetColor Foreground Dull White]
-setColor 8 = setSGR [SetColor Foreground Vivid Black]
-setColor 9 = setSGR [SetColor Foreground Vivid Blue]
-setColor 10 = setSGR [SetColor Foreground Vivid Green]
-setColor 11 = setSGR [SetColor Foreground Vivid Cyan]
-setColor 12 = setSGR [SetColor Foreground Vivid Red]
-setColor 13 = setSGR [SetColor Foreground Vivid Magenta]
-setColor 14 = setSGR [SetColor Foreground Vivid Yellow]
-setColor 15 = setSGR [SetColor Foreground Vivid White]
+getIntensity :: Int -> ColorIntensity
+getIntensity x | x > 7 = Vivid
+getIntensity _ = Dull
 
+colors = [Black, Blue, Green, Cyan, Red, Magenta, Yellow, White]
+
+getTint :: Int -> Color
+getTint x | x > 7 = getTint (x - 8)
+getTint x = colors !! x
+
+setColor :: Int -> IO ()
+setColor x =
+  let intensity = getIntensity x
+      tint = getTint x
+  in setSGR [SetColor Foreground intensity tint]
 
 printDescription :: Description -> IO ()
 printDescription (Description _ displayData _) = do
