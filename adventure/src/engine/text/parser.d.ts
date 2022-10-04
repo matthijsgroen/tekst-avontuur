01572b85bc670f5e896de0b078b255e0484c78ad
Matthijs Groen <matthijs.groen@gmail.com>
2022-10-04 21:54:39 +0200

    Move to proper repo

--- adventure/src/engine/text/parser.d.ts
+++
@@ -1,1 +0,0 @@
-export function parse(input: string): Program;
--- adventure/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { world, convertGame } from "./dsl/dsl";
-export { runGame as runCLIGame } from "./cli-client/run";
-export type { TranslationFile } from "./export-translations/exportTranslations";
-export { exportTranslations } from "./export-translations/exportTranslations";
-export { hexColor } from "./engine/hexColor";
