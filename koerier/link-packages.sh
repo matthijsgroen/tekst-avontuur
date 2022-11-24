da28947115784857c18c7bb2e05093d7c21492ea
Matthijs Groen <matthijs.groen@gmail.com>
2022-11-24 12:44:15 +0100

    Link state package

--- koerier/link-packages.sh
+++ koerier/link-packages.sh
@@ -2,6 +2,7 @@ yarn link @point-n-click/parcel-resolver
 yarn link @point-n-click/web-engine
 yarn link @point-n-click/engine
 yarn link @point-n-click/types
+yarn link @point-n-click/state
 yarn link @point-n-click/themes
 yarn link @point-n-click/theme-cli
 yarn link @point-n-click/content-description-text
